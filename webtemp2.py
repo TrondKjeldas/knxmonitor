b7d10c1a893f6fa2239f77df5929848858f9c0c4
Trond Kjeldås <trond@kjeldas.no>
2010-10-14 19:52:48 +0000

    Minor tweaks...

--- webtemp2.py
+++ webtemp2.py
@@ -32,20 +32,27 @@ floors = { "ute"     : { "rooms" : [ "ute" ] },
            "etg2"    : { "rooms" : [ "soverom", "bad", "stue2" ] } }
 
 
-rooms = { "ute"      : { "temperature" : "3/2/0",
-                         "heating"     : "" },
-          "kjøkken"  : { "temperature" : "1/3/0",
-                         "heating"     : "1/2/11" },
-          "stue"     : { "temperature" : "1/3/1",
-                         "heating"     : "1/2/6" },
-          "vindfang" : { "temperature" : "1/3/5",
-                         "heating"     : "1/2/10" },
-          "bad"      : { "temperature" : "2/3/0",
-                         "heating"     : "2/6/0" },
-          "stue2"    : { "temperature" : "2/3/1",
-                         "heating"     : "2/6/1" },
-          "soverom"  : { "temperature" : "2/3/2",
-                         "heating"     : "2/6/2" } }
+rooms = { "ute"      : { "temperature" : ("3/2/0", "temp"),
+                         "want temp"   : ("",      "temp"),
+                         "heating"     : ("","%3")           },
+          "kjøkken"  : { "temperature" : ("1/3/0", "temp"),
+                         "want temp"   : ("1/4/0", "temp"),
+                         "heating"     : ("1/2/0", "%3")},
+          "stue"     : { "temperature" : ("1/3/1", "temp"),
+                         "want temp"   : ("1/4/1", "temp"),
+                         "heating"     : ("1/2/1", "%3") },
+          "vindfang" : { "temperature" : ("1/3/5", "temp"),
+                         "want temp"   : ("1/4/5", "temp"),
+                         "heating"     : ("1/2/5", "%3")},
+          "bad"      : { "temperature" : ("2/3/0", "temp"),
+                         "want temp"   : ("2/4/0", "temp"),
+                         "heating"     : ("2/6/0", "%3")     },
+          "stue2"    : { "temperature" : ("2/3/1", "temp"),
+                         "want temp"   : ("2/4/1", "temp"),
+                         "heating"     : ("2/6/1", "%3")     },
+          "soverom"  : { "temperature" : ("2/3/2", "temp"),
+                         "want temp"   : ("2/4/2", "temp"),
+                         "heating"     : ("2/6/2", "%3")     }  }
 
 
 
@@ -74,15 +81,15 @@ def index(req):
         group_addresses = []
         types = {}
         for room in floors[floor]["rooms"]:
-            group_addr = rooms[room]["temperature"]
-            types[group_addr] = "temp"
+            group_addr, typ = rooms[room]["temperature"]
+            types[group_addr] = typ
             #return "%s %s" %(ga, types)
             group_addresses.append(group_addr)
                 
         logHandler = _regenImage(None, group_addresses, types,
                                  basedir + "../images/" + fname)
         
-        mid +=  '<a href="webtemp2/floorShow?id=%s"><img src="/images/%s" /></a>\n' %(floor,fname)
+        mid +=  '<a href="floorShow?id=%s"><img src="/images/%s" /></a>\n' %(floor,fname)
 
 
     return html_pre + mid + html_post
@@ -95,19 +102,20 @@ def floorShow(req):
     logHandler = None
     for room in floors[floor]["rooms"]:
         fname = _mkfname(room + ".png")
-        group_addresses = [ rooms[room]["temperature"], rooms[room]["heating"] ]
-        types = { rooms[room]["temperature"] : "temp",
-                  rooms[room]["heating"]     : "%" }
-                      
+        g1,t1 = rooms[room]["temperature"]
+        g2,t2 = rooms[room]["heating"]
+        #g3,t3 = rooms[room]["want temp"]
+        group_addresses = [ g1, g2 ]
+        types = { g1 : t1, g2 : t2 }
         logHandler = _regenImage(None, group_addresses, types,
-                                 basedir + "../images/" + fname)
+                                 basedir + "../images/" + fname, 22)
         
         mid +=  '<a href="webtemp2/roomShow?id=%s"><img src="/images/%s" /></a>\n' %(room,fname)
 
     return html_pre + mid + html_post
 
 
-def _regenImage(logview_instance, gas, types, imgfile):
+def _regenImage(logview_instance, gas, types, imgfile, addHorLine=None):
 
     #
     # Only re-decode if one of the .hex files are newer than the image file
@@ -128,7 +136,7 @@ def _regenImage(logview_instance, gas, types, imgfile):
         if logview_instance == None:
             logview_instance = KnxLogViewer(devices, groups, filenames,
                                             False, types, True, 0)
-        logview_instance.plotLog(gas, imgfile)
+        logview_instance.plotLog(gas, imgfile, addHorLine)
 
     return logview_instance
 
