da4e2f7d31ca0f51a04d0a283466d63cc2fe2b80
Trond Kjeldås <trond@kjeldas.no>
2018-11-26 21:39:22 +0100

    Splitted out netcat functionality to its own class, to facilitate reuse.

--- push2influx.py
+++ push2influx.py
@@ -1,49 +1,12 @@
 #!/usr/bin/python
 import sys
-import socket
 from StringIO import StringIO
 
 real_stdout = sys.stdout
 sys.stdout = StringIO()
 
 from knxmonitor import knxmonitor_decoder
-
-
-class Netcat:
-
-    """ Python 'netcat like' module """
-
-    def __init__(self, ip, port):
-
-        self.buff = ""
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((ip, port))
-
-    def read(self, length=1024):
-        """ Read 1024 bytes off the socket """
-
-        return self.socket.recv(length)
-
-    def read_until(self, data):
-        """ Read data into the buffer until we have data """
-
-        while not data in self.buff:
-            self.buff += self.socket.recv(1024)
-
-        pos = self.buff.find(data)
-        rval = self.buff[:pos + len(data)]
-        self.buff = self.buff[pos + len(data):]
-
-        return rval
-
-    def write(self, data):
-
-        self.socket.send(data)
-
-    def close(self):
-
-        self.socket.close()
-
+from knxmonitor.netcat import Netcat
 
 for a in sys.argv[1:]:
     args = [sys.argv[0], "-j"]
